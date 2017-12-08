0a93da50046a5a43325966610a63f2e12192f124
Eugene Jen <ejen@MBP0537.local>
2017-12-08 18:48:22 -0500

    crude chaser that can send a message per call.

--- src/main/java/com/eugenejen/rabbitChaser/Main.java
+++ src/main/java/com/eugenejen/rabbitChaser/Main.java
@@ -2,17 +2,68 @@ package com.eugenejen.rabbitChaser;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.listener.SimpleMessageListenerContainer;
-import org.springframework.jms.listener.adapter.MessageListenerAdapter;
+import com.codahale.metrics.MetricRegistry;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import com.rabbitmq.client.impl.StandardMetricsCollector;
+import java.net.URI;
 
 public class Main {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    private Logger logger;
+    private URI rabbitmqUri;
+    private StandardMetricsCollector metrics;
+    private CachingConnectionFactory factory;
+    private RabbitTemplate template;
 
-    public static void main(String args[]) {
-        LOGGER.info("Hello World");
-        System.exit(0);
+    Main(Logger logger, String rabbitmqUrl) throws Exception {
+        this.logger = logger;
+        this.rabbitmqUri = new URI(rabbitmqUrl);
+    }
+
+    public Main info(String fmt, Object ... args ) {
+        this.logger.info(fmt, args);
+        return this;
+    }
+
+    public Main init() {
+        MetricRegistry metricRegistry = new MetricRegistry();
+        this.metrics = new StandardMetricsCollector(metricRegistry);
+        this.factory = new CachingConnectionFactory(rabbitmqUri);
+        this.factory.getRabbitConnectionFactory().setMetricsCollector(metrics);
+        template = new RabbitTemplate(this.factory);
+        return this;
+    }
+
+    public Main startTest() {
+        this.template.convertAndSend("default", "Hello World");
+        return this;
+    }
+
+    public Main reportMetrics() {
+
+        return this;
+    }
+
+    public static void main(String args[]) throws Exception {
+        try {
+            Logger logger = LoggerFactory.getLogger(Main.class);
+            String rabbitmqUrl = System.getProperty("rabbitmqUrl", "amqp://guest:guest@localhost:5672/");
+            Main main = new Main(logger, rabbitmqUrl);
+            main.init();
+            main.info("{}", main.rabbitmqUri.toString());
+            main.info("{}", main.factory.toString());
+            main.startTest();
+            main.reportMetrics();
+            System.exit(0);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+            System.exit(1);
+        } finally {
+
+        }
     }
 
 }
