cf658c555726d78c125a43723a4c0cd95fc94cab
Eugene Jen <ejen@MBP0537.local>
2017-12-11 10:08:38 -0500

    remove trailing / that causes problem in default mode testing

--- src/main/java/com/eugenejen/rabbitChaser/Main.java
+++ src/main/java/com/eugenejen/rabbitChaser/Main.java
@@ -50,7 +50,7 @@ public class Main {
     public static void main(String args[]) throws Exception {
         try {
             Logger logger = LoggerFactory.getLogger(Main.class);
-            String rabbitmqUrl = System.getProperty("rabbitmqUrl", "amqp://guest:guest@localhost:5672/");
+            String rabbitmqUrl = System.getProperty("rabbitmqUrl", "amqp://guest:guest@localhost:5672");
             Main main = new Main(logger, rabbitmqUrl);
             main.init();
             main.info("{}", main.rabbitmqUri.toString());
