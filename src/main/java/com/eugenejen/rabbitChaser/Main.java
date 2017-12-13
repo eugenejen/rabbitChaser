d0d3acf58fdc0af33a6b409b5b6cf4d6070e8381
Eugene Jen <ejen@MBP0537.local>
2017-12-13 14:45:09 -0500

    remove unused imported packages

--- src/main/java/com/eugenejen/rabbitChaser/Main.java
+++ src/main/java/com/eugenejen/rabbitChaser/Main.java
@@ -14,8 +14,6 @@ import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.core.Message;
-import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
