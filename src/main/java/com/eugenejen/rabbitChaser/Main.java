b2f6fa28554bf0243cb4d01a155686bd3c4ff988
Matt Dudys <mdudys@sailthru.com>
2017-12-15 12:00:27 -0500

    refactor into send and drain tests

--- src/main/java/com/eugenejen/rabbitChaser/Main.java
+++ src/main/java/com/eugenejen/rabbitChaser/Main.java
@@ -1,56 +1,33 @@
 package com.eugenejen.rabbitChaser;
 
-
-import com.rabbitmq.client.impl.StandardMetricsCollector;
 import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.MetricRegistry;
-import com.codahale.metrics.Meter;
-
-import de.svenjacobs.loremipsum.LoremIpsum;
-
-import java.net.URI;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.Random;
-
-import org.slf4j.impl.SimpleLogger;
+import com.rabbitmq.client.impl.StandardMetricsCollector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
+import java.net.URI;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    private Logger logger;
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     private String mode;
     private URI rabbitmqUri;
-    private StandardMetricsCollector collector;
     private MetricRegistry registry;
     private CachingConnectionFactory factory;
-    private RabbitTemplate template;
     private ExecutorService threadPool;
-    private LoremIpsum messageGenerator;
-    private Random randomNumberGenerator;
     private ConsoleReporter reporter;
+    private Runnable test;
 
-    Main(Logger logger, String rabbitmqUrl, String mode, TestParams testParams) throws Exception {
-        this.logger = logger;
+    Main(String rabbitmqUrl, String mode, TestParams testParams) throws Exception {
         this.mode = mode;
         this.rabbitmqUri = new URI(rabbitmqUrl);
-        this.messageGenerator = new LoremIpsum();
-        this.randomNumberGenerator = new Random();
         this.registry = new MetricRegistry();
         this.init(testParams);
-    }
-
-    public Main info(String fmt, Object ... args ) {
-        this.logger.info(fmt, args);
-        return this;
-    }
-
-    public Main debug(String fmt, Object ... args ) {
-        this.logger.debug(fmt, args);
-        return this;
     }
 
     public Main init(TestParams testParams) {
@@ -58,104 +35,35 @@ public class Main {
                                        .convertRatesTo(TimeUnit.SECONDS)
                                        .convertDurationsTo(TimeUnit.MILLISECONDS)
                                        .build();
-        this.collector = new StandardMetricsCollector(this.registry);
+        StandardMetricsCollector collector = new StandardMetricsCollector(this.registry);
         this.factory = new CachingConnectionFactory(rabbitmqUri);
         factory.setCacheMode(testParams.cacheMode);
         factory.setChannelCacheSize(testParams.channelSize);
         factory.setConnectionCacheSize(testParams.connectionSize);
         this.threadPool = Executors.newFixedThreadPool(testParams.threadPoolSize);
         this.factory.getRabbitConnectionFactory().setMetricsCollector(collector);
-        template = new RabbitTemplate(this.factory);
-        return this;
+        RabbitTemplate template = new RabbitTemplate(this.factory);
 
-    }
-
-    public Main startTest(TestParams testParams) throws Exception {
-        this.reporter.start(1, TimeUnit.SECONDS);
         if (this.mode.equals("send")) {
-            this.startFiniteTest(testParams);
-        } else if (this.mode.equals("read")) {
-            this.startFiniteTest(testParams);
-        } else if (this.mode.equals("drain")) {
-            this.startIndefinteTest(testParams);
+            this.test = new Send(threadPool, template, registry, testParams);
+        } else if ("read".equals(this.mode) || "drain".equals(this.mode) ) {
+            this.test = new Drain(threadPool, registry, template, testParams);
         }
         return this;
+
     }
 
-    private Main startIndefinteTest(TestParams testParams) throws Exception {
-        for(int i = 0; i < testParams.threadPoolSize; i++) {
-            if (this.mode.equals("drain")) {
-                this.drainQueue();
-            }
-        }
+    private void runTest() throws Exception {
+        this.reporter.start(1, TimeUnit.SECONDS);
+        this.test.run();
         this.threadPool.shutdown();
         this.threadPool.awaitTermination(10, TimeUnit.MINUTES);
-        return this;
     }
 
-    private String generateMessage(TestParams testParams) {
-        int messageLength = this.randomNumberGenerator.nextInt(
-            testParams.maxMessageSizeInWords - testParams.minMessageSizeInWords
-            ) + testParams.minMessageSizeInWords;
-        return this.messageGenerator.getWords(messageLength);
-    }
-
-    private Main drainQueue() throws Exception {
-        this.threadPool.submit(
-            () -> {
-                String message;
-                do {
-                    message = (String) this.template.receiveAndConvert("default");
-                } while (message != null);
-            }
-        );
-        return this;
-    }
-
-    private Main startFiniteTest(TestParams testParams) throws Exception {
-        for(int i = 0; i < testParams.numberOfTests; i++) {
-            if (this.mode.equals("send")) {
-                this.sendToQueue(testParams);
-            } else if(this.mode.equals("read")) {
-                this.readFromQueue();
-            }
-        }
-        this.threadPool.shutdown();
-        this.threadPool.awaitTermination(10, TimeUnit.MINUTES);
-        return this;
-    }
-
-
-    private Main readFromQueue() throws Exception {
-        this.threadPool.submit(
-            () -> {
-                 this.template.receiveAndConvert("default");
-                 this.registry.getMeters().get("reqs per sec").mark();
-            }
-       );
-        return this;
-    }
-
-    private Main sendToQueue(TestParams testParams) throws Exception {
-        this.threadPool.submit(
-            () -> {
-                String message = this.generateMessage(testParams);
-                this.template.convertAndSend("default", message);
-                this.registry.getMeters().get("reqs per sec").mark();
-            }
-        );
-        return this;
-    }
-
-    public Main endTest(TestParams testParams) throws Exception {
+    public void endTest() {
         this.reporter.stop();
         this.factory.destroy();
-        return this;
-    }
-
-    public Main reportMetrics() {
         this.reporter.report();
-        return this;
     }
 
     public static void main(String args[]) throws Exception {
@@ -164,18 +72,16 @@ public class Main {
             String mode = System.getProperty("mode", "send").toLowerCase();
             String logLevel = System.getProperty("logLevel", "info").toUpperCase();
             System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, logLevel);
-            Logger logger = LoggerFactory.getLogger(Main.class);
             TestParams testParams = new TestParams();
             testParams.channelSize = Integer.parseInt(System.getProperty("channelSize", "1"));
             testParams.connectionSize = Integer.parseInt(System.getProperty("connectionSize", "1"));
             testParams.numberOfTests = Integer.parseInt(System.getProperty("numberOfTests", "1"));
             testParams.threadPoolSize = Integer.parseInt(System.getProperty("threadPoolSize", "1"));
-            Main main = new Main(logger, rabbitmqUrl, mode, testParams);
-            main.debug("{}", main.toString());
-            main.info("{}", testParams.toString());
-            main.startTest(testParams);
-            main.reportMetrics();
-            main.endTest(testParams);
+            Main main = new Main(rabbitmqUrl, mode, testParams);
+            LOGGER.debug("{}", main.toString());
+            LOGGER.info("{}", testParams.toString());
+            main.runTest();
+            main.endTest();
             System.exit(0);
         } catch (Exception e) {
             System.err.println(e.getMessage());
